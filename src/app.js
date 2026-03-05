ea7dbdcf86f8b38b7c15fc6854f12de222311255
Pedro Augusto Fernandes Francelino <pedro.francelino@aluno.senai.br>
2026-03-05 14:16:25 -0300

    CRUD de Eventos no Padrão

--- src/app.js
+++ src/app.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
 
-// Middleware para ler JSON no body das requisições
+
+// Middleware para ler JSON no body
 app.use(express.json());
 
-// Rota de teste. (Remover depois)
+
+// Importar rotas
+const eventoRoutes = require("./routes/eventoRoutes");
+
+
+// Usar rotas com prefixo
+app.use("/eventos", eventoRoutes);
+
+
+// Rota raiz (informativa)
 app.get("/", (req, res) => {
-    res.json({ mensagem: "API de Notificações funcionando!"});
+    res.json({
+        mensagem: "API de Notificações",
+        rotas: {
+            eventos: "/eventos",
+        },
+    });
 });
+
 
 module.exports = app;
